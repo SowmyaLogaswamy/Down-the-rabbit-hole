7b58cf0a83a59b0f133c466ad0fc20f419449812
Joel Bakken, Laura Hamilton, Sowmya Logaswamy and Mick Sexton <joel@>
2017-06-20 13:54:31 -0700

    add fade in and fade out elements

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,11 @@ function resetOutput() {
   $("#element1-clickbutton").hide();
   $("#element1-details").hide();
   $("#element2-trigger").hide();
+  $("#element2b-trigger").hide();
   $("#element2-details").hide();
 }
 
 $(document).ready(function() {
-
-  // $(".list-elements").click(function(event) {
-    // event.preventDefault();
-    // $(".all-element-triggers").hide();
-    // $(".all-element-details").hide();
-  //   // var $this = $(this).parent().find('span');
-  //   // $(".all-element-details p").not($this).hide();
-  // });
-
-  // $(".all-element-triggers").click(function() {
-  //   $(".all-element-details").hide();
-  // });
 
   $("li#element1-id").click(function() {
     resetOutput();
@@ -31,15 +20,15 @@ $(document).ready(function() {
 
   $("li#element2-id").click(function() {
     resetOutput();
-    // $("#element1-clickbutton").hide();
-    // $("#element1-details").hide();
     $("#element2-trigger").show();
+    $("#element2b-trigger").show();
   });
 
   $("#element2-trigger").click(function() {
-    // $("#element1-clickbutton").hide();
-    // $("#element1-details").hide();
-    $("div#element2-details").show();
+    $("div#element2-details").fadeIn();
+  });
+  $("#element2b-trigger").click(function() {
+    $("div#element2-details").fadeOut();
   });
 
 });
