1f884e731e474beef2e1e91c98789e8d24df537e
Joel Bakken, Laura Hamilton, Sowmya Logaswamy and Mick Sexton <joel@>
2017-06-20 11:40:32 -0700

    add more tweaking to hide and show functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,43 @@
 //USER INTERFACE LOGIC
+function resetOutput() {
+  $("#element1-clickbutton").hide();
+  $("#element1-details").hide();
+}
 
 $(document).ready(function() {
+
+  // $(".list-elements").click(function(event) {
+    // event.preventDefault();
+    // $(".all-element-triggers").hide();
+    // $(".all-element-details").hide();
+  //   // var $this = $(this).parent().find('span');
+  //   // $(".all-element-details p").not($this).hide();
+  // });
+
+  // $(".all-element-triggers").click(function() {
+  //   $(".all-element-details").hide();
+  // });
+
   $("li#element1-id").click(function() {
-    $("#element1-clickbutton").show();
+    // resetOutput();
+    $("#element1-clickbutton").toggle();
   });
-  $("#element1-trigger").click(function() {
-    $("#element1-details").show();
+
+  $("button#element1-clickbutton").click(function() {
+    $("div#element1-details").toggle();
+  });
+
+  $("li#element2-id").click(function() {
+    // resetOutput();
+    $("#element1-clickbutton").hide();
+    $("#element1-details").hide();
+    $("#element2-trigger").toggle();
+  });
+
+  $("#element2-trigger").click(function() {
+    // $("#element1-clickbutton").hide();
+    // $("#element1-details").hide();
+    $("div#element2-details").toggle();
   });
 
 });
