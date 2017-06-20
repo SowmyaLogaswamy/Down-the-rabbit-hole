5d9c5234dcbbaf1ec7ff0111b714185c46da08a5
Joel Bakken, Laura Hamilton, Sowmya Logaswamy and Mick Sexton <mick@>
2017-06-20 11:52:04 -0700

    add back reset function and adjust toggles to shows, now that triggers are outside of list items, its working

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 function resetOutput() {
   $("#element1-clickbutton").hide();
   $("#element1-details").hide();
+  $("#element2-trigger").hide();
+  $("#element2-details").hide();
 }
 
 $(document).ready(function() {
@@ -19,25 +21,25 @@ $(document).ready(function() {
   // });
 
   $("li#element1-id").click(function() {
-    // resetOutput();
-    $("#element1-clickbutton").toggle();
+    resetOutput();
+    $("#element1-clickbutton").show();
   });
 
   $("button#element1-clickbutton").click(function() {
-    $("div#element1-details").toggle();
+    $("div#element1-details").show();
   });
 
   $("li#element2-id").click(function() {
-    // resetOutput();
-    $("#element1-clickbutton").hide();
-    $("#element1-details").hide();
-    $("#element2-trigger").toggle();
+    resetOutput();
+    // $("#element1-clickbutton").hide();
+    // $("#element1-details").hide();
+    $("#element2-trigger").show();
   });
 
   $("#element2-trigger").click(function() {
     // $("#element1-clickbutton").hide();
     // $("#element1-details").hide();
-    $("div#element2-details").toggle();
+    $("div#element2-details").show();
   });
 
 });
