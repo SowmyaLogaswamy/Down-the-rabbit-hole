6ef4b1cb32bc174a69712791f517861cf82ce873
Joel Bakken, Laura Hamilton, Sowmya Logaswamy and Mick Sexton <mick@>
2017-06-20 10:07:52 -0700

    add functionality to hide/show trigger for a list item and details for an element

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//USER INTERFACE LOGIC
+
+$(document).ready(function() {
+  $("li#element1-id").click(function() {
+    $("#element1-clickbutton").show();
+  });
+  $("#element1-trigger").click(function() {
+    $("#element1-details").show();
+  });
+
+});
